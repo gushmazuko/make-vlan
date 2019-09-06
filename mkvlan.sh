5f470c6b7253ca80aeb1b8f207e6d210805a6b2d
mg.stratzone <gushmazuko@protonmail.com>
2019-09-06 20:54:28 +0200

    Update

--- mkvlan.sh
+++ mkvlan.sh
@@ -1,31 +1,31 @@
 #!/usr/bin/env bash
 
-int_name="eth0"
-VLAN_ID="${1}"
+INT_NAME="${1}"
+VLAN_ID="${2}"
 
 manual() {
-	sudo ip link add link ${int_name} name ${int_name}.$VLAN_ID type vlan id $VLAN_ID
+	ip link add link ${INT_NAME} name ${INT_NAME}.$VLAN_ID type vlan id $VLAN_ID
 }
 
 dhcp() {
-	sudo dhclient ${int_name}.$VLAN_ID
+	dhclient ${INT_NAME}.$VLAN_ID
 }
 
 delete(){
-	sudo ip link del link ${int_name} name ${int_name}.$VLAN_ID
+	ip link del link ${INT_NAME} name ${INT_NAME}.$VLAN_ID
 }
 
 
-if [ -z "$VLAN_ID" ]
+if [ -z "$INT_NAME" ] || [ -z "$VLAN_ID" ]
 then
-	echo "Usage: $0 [ VLAN ID ] { m[anual] | dh[cp] | d[el] }
+	echo "Usage: $0 [ INTERFACE NAME ] [ VLAN ID ] { m[anual] | dh[cp] | d[el] }
 Example for create vlan 10 with dhcp: $0 10 dh"
 else
-	case "$2" in
+	case "$3" in
 		m|manual)		manual ;;
 		dh|dhcp)		manual; dhcp ;;
-		d|del)		delete ;;
-		*)			echo "Usage: $0 [ VLAN ID ] m[anual] | dh[cp] | d[el]" >&2
+		d|del)			delete ;;
+		*)			echo "Usage: $0 [ INTERFACE NAME ] [ VLAN ID ] m[anual] | dh[cp] | d[el]" >&2
 					exit 1 ;;
 	esac
 fi
